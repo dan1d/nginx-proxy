b8fafcd56588fe99905aa98a09a6fb7bf6db2339
Daniel Dominguez <danielfromarg@gmail.com>
2023-01-25 14:46:00 -0300

    start srcrypt

--- start.sh
+++ start.sh
@@ -3,19 +3,22 @@ echo starting apps
 
 echo starting porfolio 
 cd ../portfolio-dan1d
-docker-compose up -d
-
+docker-compose down
+docker-compose -f docker-compose.prod.yml up -d
 
 echo starting pictures
 cd ../p1ctures
-docker-compose -f docker-compose.prod.yml up -d
+docker-compose down
+docker-compose up -d
 
 echo starting euca-estudio
 cd ../euca-estudio
+docker-compose down
 docker-compose up -d
 
 echo starting reverse proxy nginx
 cd ../nginx-proxy
+docker-compose down
 docker-compose up -d
 
 
