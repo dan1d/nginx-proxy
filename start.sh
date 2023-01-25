34a2cb52b89008b53eaa8a019b30f91b2eb4f61b
Daniel Dominguez <danielfromarg@gmail.com>
2023-01-25 14:39:50 -0300

    update dc alias

--- start.sh
+++ start.sh
@@ -3,18 +3,18 @@ echo starting apps
 
 echo starting porfolio api
 cd ../porfolio
-dc up -d
+docker-compose up -d
 
 echo starting porfolio FE
 cd ../portfolio-dan1d
-dc up -d
+docker-compose up -d
 
 echo starting pictures
 cd ../p1ctures
-dc up -d
+docker-compose up -d
 
 echo starting reverse proxy nginx
 cd ../nginx-proxy
-dc up -d
+docker-compose up -d
 
 
